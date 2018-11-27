8d04d772754d4013334a91c9fa074f7ed445718a
yngtodd <young.todd.mk@gmail.com>
2018-11-26 22:24:26 -0500

    Adding launch script to evaluate hyperband on ppo

--- launch_hyperband_ppo.sh
+++ launch_hyperband_ppo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-BATCH_SIZE=1
+BATCH_SIZE=16
 COUNTER=0
 SEED=1000
-CUDA=0
+CUDA=9
 
-for i in {1..10}
+for i in {1..100}
 do
     if [[ $COUNTER > $BATCH_SIZE ]]; then CUDA=$((CUDA+1)); COUNTER=$((0)); fi
-    vel /home/ygx/src/vel/examples-configs/rl/atari/journal/ppo/hyperspace_qbert_ppo.yaml train -r $i -s $SEED -d cuda:$CUDA &
+    vel /Users/yngtodd/src/develop/vel/examples-configs/rl/atari/journal/ppo/hyperband_qbert_ppo.yaml train -r $i -s $SEED -d cuda:$CUDA &
     COUNTER=$((COUNTER+1))
     SEED=$((SEED+1))
     echo $CUDA
