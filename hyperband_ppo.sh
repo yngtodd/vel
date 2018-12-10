f75b745ab1dd254261ea077aa1c9082df5c6d721
Young M T <ygx@dgx2-B.ornl.gov>
2018-12-10 15:57:00 -0500

    Big backup

---
+++ hyperband_ppo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BATCH_SIZE=6
+COUNTER=0
+SEED=1000
+CUDA=8
+
+for i in {51..100}
+do
+    if [ $COUNTER -gt $BATCH_SIZE ]; then CUDA=$((CUDA+1)); COUNTER=$((0)); fi
+    vel /home/ygx/src/vel/examples-configs/rl/atari/journal/ppo/hyperband_qbert_ppo.yaml train -r $i -s $SEED -d cuda:$CUDA &
+    COUNTER=$((COUNTER+1))
+    #echo $CUDA
+    SEED=$((SEED+1))
+done
